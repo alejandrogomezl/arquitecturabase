8776bb6c47cac5b607b00ac8bbffefb4b7ae16a5
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2026-05-31 00:39:55 +0200

    Bloque 2.3: implementar la capa de acceso a datos (CAD) con MongoDB

--- servidor/cad.js
+++ servidor/cad.js
@@ -1,17 +1,79 @@
 const mongo=require("mongodb").MongoClient;
+const ObjectId=require("mongodb").ObjectId;
+
+function buscar(coleccion,criterio,callback){
+    coleccion.find(criterio).toArray(function(error,usuarios){
+        if(usuarios.length==0){
+            callback(undefined);
+        }
+        else{
+            callback(usuarios[0]);
+        }
+    });
+}
+
+function insertar(coleccion,elemento,callback){
+    coleccion.insertOne(elemento,function(err,result){
+        if(err){
+            console.log("error");
+        }
+        else{
+            console.log("Nuevo elemento creado");
+            callback(elemento);
+        }
+    });
+}
+
+function actualizar(coleccion,obj,callback){
+    coleccion.findOneAndUpdate(
+        {_id:ObjectId(obj._id)},
+        {$set:obj},
+        {upsert:false,returnDocument:"after",projection:{email:1}},
+        function(err,doc){
+            if(err){ throw err; }
+            else{
+                console.log("Elemento actualizado");
+                callback({email:doc.value.email});
+            }
+        }
+    );
+}
 
 function CAD(){
-  this.usuarios=null;
+    this.usuarios=null;
 
-  this.conectar=async function(callback){
-    let cad=this;
-    let conStr=process.env.connectionString;
-    let client=new mongo(conStr);
-    await client.connect();
-    const database=client.db("sistema");
-    cad.usuarios=database.collection("usuarios");
-    callback(database);
-  }
+    this.conectar=async function(callback){
+        let cad=this;
+        let conStr=process.env.MONGODB_URI;
+        let client=new mongo(conStr);
+        await client.connect();
+        const database=client.db("sistema");
+        cad.usuarios=database.collection("usuarios");
+        callback(database);
+    };
+
+    this.buscarOCrearUsuario=function(usr,callback){
+        this.usuarios.findOneAndUpdate(
+            {email:usr.email},
+            {$setOnInsert:{email:usr.email,nick:usr.email}},
+            {upsert:true,returnDocument:"after"},
+            function(err,doc){
+                callback(doc.value);
+            }
+        );
+    };
+
+    this.buscarUsuario=function(obj,callback){
+        buscar(this.usuarios,obj,callback);
+    };
+
+    this.insertarUsuario=function(usuario,callback){
+        insertar(this.usuarios,usuario,callback);
+    };
+
+    this.actualizarUsuario=function(obj,callback){
+        actualizar(this.usuarios,obj,callback);
+    };
 }
 
 module.exports.CAD=CAD;
--- servidor/modelo.js
+++ servidor/modelo.js
@@ -1,4 +1,6 @@
 const datos=require("./cad.js");
+const bcrypt=require("bcrypt");
+const correo=require("./email.js");
 
 function Sistema(test){
     this.usuarios={};
@@ -30,6 +32,73 @@ function Sistema(test){
 
     this.numeroUsuarios=function(){
         return Object.keys(this.usuarios).length;
+    };
+
+    this.usuarioGoogle=function(obj,callback){
+        this.cad.buscarOCrearUsuario(obj,function(usr){
+            callback(usr);
+        });
+    };
+
+    this.registrarUsuario=async function(obj,callback){
+        let modelo=this;
+        if(!obj.nick){
+            obj.nick=obj.email;
+        }
+        this.cad.buscarUsuario({email:obj.email},async function(usr){
+            if(!usr){
+                let key=Date.now().toString();
+                obj.confirmada=false;
+                obj.key=key;
+                const hash=await bcrypt.hash(obj.password,10);
+                obj.password=hash;
+                modelo.cad.insertarUsuario(obj,function(res){
+                    callback(res);
+                });
+                correo.enviarEmail(obj.email,key,"Confirmar cuenta").catch(err=>{
+                    console.log("Error enviando email:",err.message);
+                });
+            }
+            else{
+                callback({email:-1});
+            }
+        });
+    };
+
+    this.confirmarUsuario=function(obj,callback){
+        let modelo=this;
+        this.cad.buscarUsuario({email:obj.email,confirmada:false,key:obj.key},function(usr){
+            if(usr){
+                usr.confirmada=true;
+                modelo.cad.actualizarUsuario(usr,function(res){
+                    callback({email:res.email});
+                });
+            }
+            else{
+                callback({email:-1});
+            }
+        });
+    };
+
+    this.loginUsuario=function(obj,callback){
+        let modelo=this;
+        this.cad.buscarUsuario({email:obj.email,confirmada:true},function(usr){
+            if(!usr){
+                callback({email:-1});
+                return -1;
+            }
+            else{
+                bcrypt.compare(obj.password,usr.password,function(err,result){
+                    if(result){
+                        callback(usr);
+                        modelo.agregarUsuario(usr.nick||usr.email);
+                    }
+                    else{
+                        callback({email:-1});
+                    }
+                });
+            }
+        });
     };
 
     if(!test.test){
